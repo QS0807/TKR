17d0eb734a472ad8321e874554b0bb0a1783cdf7
Xizhe Li <jasperqs7@gmail.com>
2023-07-12 13:56:00 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// Function to handle form submission
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    // Get form values
+    let firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
+    let gender = document.getElementById('gender').value;
+
+    // Create JSON object
+    let userInformation = {
+        firstName: firstName,
+        lastName: lastName,
+        gender: gender
+    };
+
+    // Send JSON directly to the Cloudflare Worker
+    fetch('', {
+            method: 'POST',
+            mode: 'no-cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userInformation)
+        }
+    );
+}
+
+document.getElementById('userInfoForm').addEventListener('submit', handleFormSubmit);
