7944ae2cb6a9949e87b6634acdf46cab8f1ddff4
Haolinzzz <96487104+Haolinzzz@users.noreply.github.com>
2023-08-14 22:09:31 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -130,13 +130,11 @@ async function save() {
 
     //added on 8_14
 
-    let resultdata = new FormData();
-    resultdata.append(result);
-    console.log(resultdata);
+
     fetch('https://damp-bonus-34d1.hlzhong1130.workers.dev/submit', {
             method: 'POST',
             mode: 'no-cors',
-            body: result
+            body: JSON.stringify(result)
         }
     );
 }
